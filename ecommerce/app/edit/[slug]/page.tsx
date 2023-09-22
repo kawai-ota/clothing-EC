8e056773f609cfa9b4bc11838c38eb7e363979e8
河合桜汰 <o.k.20020929@icloud.com>
2023-09-23 03:34:24 +0900

    商品の詳細ページから説明を編集できるようにdbから値を見つけてくるコードを書きました

---
+++ ecommerce/app/edit/[slug]/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import prisma from "@/app/lib/prismadb";
+import Edit from "../Edit";
+
+type EditProps = {};
+
+const page = async ({ params }: { params: { slug: string } }) => {
+  const productId = parseInt(params.slug, 10);
+
+  if (isNaN(productId)) {
+    return <div>登録されていない商品番号です</div>;
+  }
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+    if (!product) {
+      return <div>この商品は登録されていません</div>;
+    }
+
+    return (
+      <div>
+        <Edit {...product} />
+      </div>
+    );
+  } catch (error) {
+    console.log(error);
+    return (
+      <div>商品の取得中にエラーが起きました。もう一度やり直してください</div>
+    );
+  }
+  return <div>page</div>;
+};
+
+export default page;
