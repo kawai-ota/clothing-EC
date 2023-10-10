5103d29dd9bde34e0c1cdaab1e5dc447a4904442
河合桜汰 <o.k.20020929@icloud.com>
2023-10-10 19:50:12 +0900

    UIを少し変更した

--- ecommerce/app/mybrands/page.tsx
+++ ecommerce/app/mybrands/page.tsx
@@ -11,6 +11,7 @@ type MyBrandsProps = {};
 
 const page = async (props: MyBrandsProps) => {
   const session = await getServerSession(options);
+
   const allmyproduct = await prisma.product.findMany({
     where: {
       userId: session?.user.id,
@@ -36,34 +37,32 @@ const page = async (props: MyBrandsProps) => {
             className="relative flex items-center justify-between w-8/12 px-6 mx-auto shadow-lg shadow-purple-100 p-5 rounded-lg mt-10"
           >
             <div>
-              <h1 className="text-2xl mb-3">{product.title}</h1>
-              <h1 className="text-2xl mb-3">値段:{product.price}</h1>
-              <h1 className="text-2xl mb-3">カテゴリー:{product.category}</h1>
-              <h1 className="text-2xl mb-3">スタイル:{product.style}</h1>
-              <h1 className="text-2xl mb-3">
-                アパレル名、店舗名:{product.store}
-              </h1>
+              <h1 className="mb-3">{product.title}</h1>
+              <h1 className="mb-3"> 値段: {product.price}</h1>
+              <h1 className="mb-3"> カテゴリー: {product.category}</h1>
+              <h1 className="mb-3"> スタイル: {product.style}</h1>
+              <h1 className="mb-3"> アパレル名: {product.store}</h1>
               <DeleteProduct productId={product.id} userId={product.userId} />
-              <Link href={`/dashboard/${product.id}`}>
-                <div>
-                  <img
-                    src={product.images.split(",")[0]}
-                    alt=""
-                    className="w-[200px] h-[200px] object-cover object-top"
-                  />
-                </div>
-              </Link>
-              {session?.user.id === product.userId && (
-                <Link
-                  className="absolute top-0 right-0"
-                  href={`/edit/${product.id}`}
-                >
-                  <span className="absolute top-0 right-0 p-2 bg-green-600 rounded-full text-white cursor-pointer">
-                    <AiTwotoneEdit size={24} />
-                  </span>
-                </Link>
-              )}
             </div>
+            <Link href={`/dashboard/${product.id}`}>
+              <div>
+                <img
+                  className="w-[200px] h-[200px] object-cover object-top"
+                  src={product.images.split(",")[0]}
+                  alt=""
+                />
+              </div>
+            </Link>
+            {session?.user.id === product.userId && (
+              <Link
+                className="absolute top-0 right-0"
+                href={`/edit/${product.id}`}
+              >
+                <span className="absolute top-0 right-0 p-2 bg-green-600 rounded-full text-white cursor-pointer">
+                  <AiTwotoneEdit size={24} />
+                </span>
+              </Link>
+            )}
           </div>
         ))}
       </div>
