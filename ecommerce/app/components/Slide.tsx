7322977a86b6476825434da7cc36d0d786303ace
河合桜汰 <o.k.20020929@icloud.com>
2023-10-18 05:56:53 +0900

    スナップのスライドショーを作成したのでコミットする

--- ecommerce/app/components/Slide.tsx
+++ ecommerce/app/components/Slide.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
+import { FiChevronLeft } from "react-icons/fi";
 
 type SlideTypeProps = {
   id?: number;
@@ -16,6 +19,19 @@ type SlideTypeProps = {
 };
 
 const Slide = (props: SlideTypeProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const imageUrls = props.images?.split(",") || [];
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div>
       <div className="flex flex-col items-center space-x-5 mb-10">
@@ -24,10 +40,24 @@ const Slide = (props: SlideTypeProps) => {
           <span className="font-medium text-xl">スナップ</span>
         </div>
         <img
-          src={props.images?.split(",").pop()}
+          src={imageUrls[currentIndex]}
           className="max-h-[300px] w-10/12 rounded-lg object-contain"
           alt=""
         />
+        <div className="flex justify-between w-10/12 mt-2">
+          <div className="flex flex-row opacity-70">
+            <button onClick={prevSlide} className="flex flex-row">
+              <FiChevronLeft className="mr-2" size={24} />
+              前へ
+            </button>
+          </div>
+          <div className="opacity-70">
+            <button onClick={nextSlide} className="flex flex-row">
+              次へ
+              <FiChevronRight size={24} className="ml-2" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
