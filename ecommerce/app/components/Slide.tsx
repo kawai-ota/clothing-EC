d5dbc6bb3e117c3081ff43ffce498c0c358d0daa
河合桜汰 <o.k.20020929@icloud.com>
2023-10-19 05:52:00 +0900

    UIの変更をした

--- ecommerce/app/components/Slide.tsx
+++ ecommerce/app/components/Slide.tsx
@@ -35,7 +35,7 @@ const Slide = (props: SlideTypeProps) => {
   return (
     <div>
       <div className="flex flex-col items-center space-x-5 mb-10">
-        <div className="flex flex-row mb-5">
+        <div className="flex flex-row mb-5 lg:mt-0 mt-20">
           <span className="w-[5px] h-[30px] bg-[#3EBCB5] rounded-full inline-block mr-5" />
           <span className="font-medium text-xl">スナップ</span>
         </div>
