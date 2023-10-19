92eaa743fd43aeaddb0d1f375f017ec8ddd52149
河合桜汰 <o.k.20020929@icloud.com>
2023-10-19 10:38:42 +0900

    編集できない部分を編集できるようにした

--- ecommerce/app/edit/Edit.tsx
+++ ecommerce/app/edit/Edit.tsx
@@ -1,14 +1,15 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Navbar from "../components/Navbar";
+import { useRouter } from "next/navigation";
 import Size from "../components/Size";
 import Color from "../components/Color";
 import Para from "../components/Para";
 import ImageUpload from "../components/ImageUpload";
 
-type EditProps = {
+interface EditProps {
   id: number;
   title: string;
   description: string;
@@ -21,7 +22,7 @@ type EditProps = {
   price: number;
   images: string;
   userId: number;
-};
+}
 
 const Edit = ({
   id,
@@ -51,9 +52,8 @@ const Edit = ({
     color: color,
     price: price,
     images: images,
-    userId: userId,
+    userId: Id,
   });
-
   const [Description, setDescription] = useState<string>("");
   const [info, setInfo] = useState<any>();
   const [imageUrls, setImageUrls] = useState<string[]>([]);
@@ -64,6 +64,7 @@ const Edit = ({
       setImageUrls(imageUrlArray);
     }
   }, []);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
@@ -71,7 +72,6 @@ const Edit = ({
       [name]: value,
     });
   };
-
   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value =
       e.target.name === "price"
@@ -92,6 +92,7 @@ const Edit = ({
     const stringImages = JSON.stringify(imageUrls);
     setFormData({
       ...formData,
+      images: stringImages,
       description: Description,
       userId: id,
     });
@@ -106,11 +107,13 @@ const Edit = ({
     }));
   }, [imageUrls]);
 
-  const updateDate = async () => {
+  const updateData = async () => {
     handleImageChange();
     try {
       const response = await axios.patch("/api/updateproduct", formData);
+      console.log(response.data);
       router.push("/");
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -122,131 +125,133 @@ const Edit = ({
         <Navbar />
       </div>
       <h1 className="text-3xl font-semibold py-6">ブランドの出品</h1>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
-        <div>
-          <label htmlFor="title" className="font-medium">
-            商品名
-          </label>
-          <input
-            type="text"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="category" className="font-medium">
-            カテゴリー
-          </label>
-          <input
-            type="text"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="category"
-            value={formData.category}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="style" className="font-medium">
-            スタイル
-          </label>
-          <input
-            type="text"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="style"
-            value={formData.style}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="store" className="font-medium">
-            ブランド名
-          </label>
-          <input
-            type="text"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="store"
-            value={formData.store}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="size" className="font-medium">
-            サイズ
-          </label>
-          <input
-            type="text"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="size"
-            value={formData.size}
-            onChange={handleChange}
-          />
-          <Size setFormData={setFormData} />
-        </div>
-        <div>
-          <label htmlFor="inventory" className="font-medium">
-            在庫
-          </label>
-          <input
-            type="number"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="inventory"
-            value={formData.inventory}
-            onChange={handlePriceChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="price" className="font-medium">
-            値段
-          </label>
-          <input
-            type="number"
-            className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-            name="price"
-            value={formData.price}
-            onChange={handlePriceChange}
-          />
-        </div>
-        <div>
+      <div className="text-black mt-4">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
           <div>
-            <label htmlFor="color" className="font-medium">
-              カラー
+            <label htmlFor="title" className="font-medium">
+              商品名
             </label>
             <input
               type="text"
-              className="w-full h-[50px] border-[1px] rounded-lg focus:border-[#3EBCB5] px-3 focus:border-2 outline-none"
-              name="color"
-              value={formData.color}
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="title"
+              value={formData.title}
               onChange={handleChange}
             />
           </div>
-          <Color setFormData={setFormData} Color={formData.color} />
+          <div>
+            <label htmlFor="category" className="font-medium">
+              カテゴリー
+            </label>
+            <input
+              type="text"
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="style" className="font-medium">
+              スタイル
+            </label>
+            <input
+              type="text"
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="style"
+              value={formData.style}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="store" className="font-medium">
+              ブランド名
+            </label>
+            <input
+              type="text"
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="store"
+              value={formData.store}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="size" className="font-medium">
+              サイズ
+            </label>
+            <input
+              type="text"
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="size"
+              value={formData.size}
+              onChange={handleChange}
+            />
+            <Size setFormData={setFormData} />
+          </div>
+          <div>
+            <label htmlFor="inventory" className="font-medium">
+              Inventory
+            </label>
+            <input
+              type="number"
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="inventory"
+              value={formData.inventory}
+              onChange={handlePriceChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="price" className="font-medium">
+              値段
+            </label>
+            <input
+              type="number"
+              className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+              name="price"
+              value={formData.price}
+              onChange={handlePriceChange}
+            />
+          </div>
+          <div>
+            <div>
+              <label htmlFor="color" className="font-medium">
+                カラー
+              </label>
+              <input
+                type="text"
+                className="w-full h-[50px] border-[1px] rounded-lg focus:border-pink-500 px-3 focus:border-2 outline-none"
+                name="color"
+                value={formData.color}
+                onChange={handleChange}
+              />
+            </div>
+            <Color setFormData={setFormData} Color={formData.color} />
+          </div>
         </div>
+        <label htmlFor="" className="mt-10 inline-block font-medium">
+          商品説明を追加
+        </label>
+        <Para
+          setDescription={setDescription}
+          description={formData.description}
+        />
+        <label htmlFor="" className="mt-10 inline-block font-medium">
+          商品画像の追加
+        </label>
+        <ImageUpload
+          info={info}
+          updateInfo={setInfo}
+          imageUrls={imageUrls}
+          setImageUrls={setImageUrls}
+          handleImageChange={handleImageChange}
+        />
+        <button
+          onClick={updateData}
+          className="text-white mt-10 border-[1px] bg-[#3EBCB5] rounded-lg px-5 p-2"
+        >
+          編集
+        </button>
       </div>
-      <label htmlFor="" className="mt-10 inline-block font-medium">
-        商品説明を追加
-      </label>
-      <Para
-        setDescription={setDescription}
-        description={formData.description}
-      />
-      <label htmlFor="" className="mt-10 inline-block font-medium">
-        商品画像の追加
-      </label>
-      <ImageUpload
-        info={info}
-        updateInfo={setInfo}
-        imageUrls={imageUrls}
-        setImageUrls={setImageUrls}
-        handleImageChange={handleImageChange}
-      />
-      <button
-        onClick={updateDate}
-        className="text-white mt-10 border-[1px] bg-[#3EBCB5] rounded-lg px-5 p-2"
-      >
-        編集
-      </button>
     </div>
   );
 };
