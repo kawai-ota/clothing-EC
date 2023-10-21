c7875c1f1fe1a77e412ea419884c9c483545801d
河合桜汰 <o.k.20020929@icloud.com>
2023-10-22 06:10:39 +0900

    カタログ部分のレスポンシブ対応と同時にUIも新規のものを実装しました

--- ecommerce/app/components/container/Item.tsx
+++ ecommerce/app/components/container/Item.tsx
@@ -18,30 +18,36 @@ const Item = async (props: ItemProps) => {
   const formatPrice = (price: number) => {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
+
   return (
-    <div>
-      <h1 className="py-3 font-medium text-3xl ml-20 mt-10 mb-10">商品</h1>
-      <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-20 ml-20">
+    <div className="px-5 max-w-full mb-10 mx-auto">
+      <h1 className="py-3 font-medium text-3xl mt-10 mb-10 text-center mx-8">
+        商品
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {products.map((product) => (
-          <div key={product.id}>
+          <div
+            key={product.id}
+            className="rounded-lg shadow-md w-full sm:w-auto bg-white mx-8"
+          >
             <Link href={`/dashboard/${product.id}`}>
-              <div className="relative rounded-lg group">
-                <img
-                  src={product.images.split(",")[0]}
-                  className="w-[250px] h-[250px] object-cover object-top rounded-lg transform group-hover:translate-y-[-10px] transition-transform  border-2 border-transparent group-hover:border-[#31ACA3] transition-border duration-300"
-                  alt=""
-                />
-              </div>
-              <div className="flex items-center justify-between mt-4">
-                <div>
-                  <h1 className="text-[14px] font-medium max-w-[150px] whitespace-nowrap overflow-hidden">
-                    {product.title}
-                  </h1>
-                  <p className="text-[13px] opacity-60">{product.store}</p>
+              <div className="relative rounded-t-lg group">
+                <div style={{ height: "200px" }}>
+                  <img
+                    src={product.images.split(",")[0]}
+                    className="w-full h-full object-cover rounded-t-lg transform group-hover:scale-105 transition-transform"
+                    alt=""
+                  />
                 </div>
-                <span className="px-2 font-medium">
-                  ￥{formatPrice(product.price)}
-                </span>
+              </div>
+              <div className="p-4">
+                <h1 className="text-lg font-medium mb-2">{product.title}</h1>
+                <p className="text-sm text-gray-600">{product.store}</p>
+                <div className="flex justify-between items-center mt-2">
+                  <span className="text-xl font-medium text-[#31ACA3]">
+                    ￥{formatPrice(product.price)}
+                  </span>
+                </div>
               </div>
             </Link>
           </div>
