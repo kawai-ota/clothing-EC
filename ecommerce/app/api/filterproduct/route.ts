793ab0c182d0731c6c3a1adc5606585316947a57
河合桜汰 <o.k.20020929@icloud.com>
2023-10-23 03:31:16 +0900

    build通るかのテスト

--- ecommerce/app/api/filterproduct/route.ts
+++ ecommerce/app/api/filterproduct/route.ts
@@ -1,35 +1,48 @@
 import prisma from "@/app/lib/prismadb";
 import { NextResponse } from "next/server";
-export const dynamic = "auto";
 // 'auto' | 'force-dynamic' | 'error' | 'force-static'
 
 export async function GET(request: Request) {
   try {
-    const searchParams = new URLSearchParams(request.credentials);
-
+    const searchParams = new URLSearchParams(request.url.split("?")[1]);
     const categories = searchParams.getAll("categories[]");
     const colors = searchParams.getAll("colors[]");
-    const sizes = searchParams.getAll("size[]");
-
-    const minPrice = parseInt(searchParams.get("price[min]") || "0");
-    const maxPrice = parseInt(searchParams.get("price[max]") || "100000");
+    let sizes = searchParams.getAll("size[]");
+    const minPriceStr = searchParams.get("price[min]");
+    const maxPriceStr = searchParams.get("price[max]");
+    const minPrice = minPriceStr ? parseInt(minPriceStr) : undefined;
+    const maxPrice = maxPriceStr ? parseInt(maxPriceStr) : undefined;
 
     const products = await prisma.product.findMany({
       where: {
         OR: [
           ...categories.map((category) => ({
-            category: { contains: category },
+            category: {
+              contains: category,
+            },
           })),
-          ...sizes.map((size) => ({ size: { contains: size } })),
-          ...colors.map((color) => ({ color: { contains: color } })),
-          { price: { gte: minPrice, lte: maxPrice } },
+          ...sizes.map((size) => ({
+            size: {
+              contains: size,
+            },
+          })),
+          ...colors.map((color) => ({
+            color: {
+              contains: color,
+            },
+          })),
+          {
+            price: {
+              gte: minPrice,
+              lte: maxPrice,
+            },
+          },
         ],
       },
     });
-
-    return NextResponse.json({ message: "API route is working" });
+    return NextResponse.json(products);
   } catch (error) {
-    console.error("Error selecting product", error);
+    console.log("商品の取得でエラーが起きました", error);
     return NextResponse.error();
   }
 }
