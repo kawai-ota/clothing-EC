89014da9b0c2759a984e9633661b0e7318872bb4
河合桜汰 <o.k.20020929@icloud.com>
2023-10-23 04:47:26 +0900

    moduleにした

--- ecommerce/app/api/color/route.ts
+++ ecommerce/app/api/color/route.ts
@@ -1,5 +1,5 @@
-// import prisma from "@/app/lib/prismadb";
-// import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prismadb";
+import { NextResponse } from "next/server";
 
 // export async function GET(request: Request) {
 //   try {
