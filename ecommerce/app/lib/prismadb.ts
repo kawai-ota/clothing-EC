e39a111359a009050c4333bcda2829fb26da8a50
河合桜汰 <o.k.20020929@icloud.com>
2023-10-24 08:20:36 +0900

    修正

--- ecommerce/app/lib/prismadb.ts
+++ ecommerce/app/lib/prismadb.ts
@@ -4,7 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const prismadb = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
-export default client;
+export default prismadb;
