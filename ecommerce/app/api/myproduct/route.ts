cb39b234ef73735ca28c35d269ce8c5ffec9126c
河合桜汰 <o.k.20020929@icloud.com>
2023-10-25 08:45:07 +0900

    エラーの修正をしました

---
+++ ecommerce/app/api/myproduct/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/app/lib/prismadb";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+
+const getMyProduct = async () => {
+  try {
+    const session = await getServerSession(options);
+    const allmyproduct = await prisma.product.findMany({
+      where: {
+        userId: session?.user.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 32000,
+    });
+    return allmyproduct;
+  } catch (error: any) {
+    return [];
+  }
+};
+
+export default getMyProduct;
