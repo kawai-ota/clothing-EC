a6de7d37e2c5105fdb3a09453a11dfd873e6c74d
河合桜汰 <o.k.20020929@icloud.com>
2023-10-25 06:06:26 +0900

    例外を処理できるようにcatchをつけた

--- ecommerce/app/api/checkout/route.ts
+++ ecommerce/app/api/checkout/route.ts
@@ -18,51 +18,56 @@ export async function OPTIONS() {
 }
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const { productIds, userId } = await body;
+  try {
+    const body = await req.json();
+    const { productIds, userId } = await body;
 
-  if (!productIds || productIds.length === 0) {
-    return new NextResponse("商品が見つかりません", { status: 400 });
-  }
+    if (!productIds || productIds.length === 0) {
+      return new NextResponse("商品が見つかりません", { status: 400 });
+    }
 
-  const products = await prisma.product.findMany({
-    where: {
-      id: {
-        in: productIds,
-      },
-    },
-    take: 32000,
-  });
-
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: "JPY",
-        product_data: {
-          name: product.title,
+    const products = await prisma.product.findMany({
+      where: {
+        id: {
+          in: productIds,
         },
-        unit_amount: product.price,
+      },
+      take: 32000,
+    });
+
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+    products.forEach((product) => {
+      line_items.push({
+        quantity: 1,
+        price_data: {
+          currency: "JPY",
+          product_data: {
+            name: product.title,
+          },
+          unit_amount: product.price,
+        },
+      });
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      billing_address_collection: "auto",
+      phone_number_collection: {
+        enabled: false,
+      },
+      success_url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/cart?success=1`,
+      cancel_url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/cart?cancelled=1`,
+      metadata: {
+        productIds: JSON.stringify(productIds),
+        userId: userId,
       },
     });
-  });
 
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: "payment",
-    billing_address_collection: "auto",
-    phone_number_collection: {
-      enabled: false,
-    },
-    success_url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/cart?success=1`,
-    cancel_url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/cart?cancelled=1`,
-    metadata: {
-      productIds: JSON.stringify(productIds),
-      userId: userId,
-    },
-  });
-
-  return NextResponse.json({ url: session.url }, { headers: corsHeader });
+    return NextResponse.json({ url: session.url }, { headers: corsHeader });
+  } catch (error) {
+    console.error("エラー:", error);
+    return new NextResponse("エラーが発生しました", { status: 500 });
+  }
 }
