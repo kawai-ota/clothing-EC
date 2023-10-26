2939bb86e8ae2764e2c8349803f32bc37826d9b2
河合桜汰 <o.k.20020929@icloud.com>
2023-10-27 01:57:34 +0900

    ログインもできないので修正

--- ecommerce/app/api/register/route.ts
+++ ecommerce/app/api/register/route.ts
@@ -1,27 +1,21 @@
+import prisma from "@/app/lib/prismadb";
 import bcrypt from "bcrypt";
-
-import prisma from "../../lib/prismadb";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
   try {
-    const body = await request.json();
-    const { email, name, password } = body;
-
-    if (!email || !name || !password) {
-      return new NextResponse("入力情報が欠けています", { status: 400 });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
     const user = await prisma.user.create({
       data: {
-        email,
         name,
+        email,
         password: hashedPassword,
       },
     });
-
     return NextResponse.json(user);
   } catch (error: any) {
     console.log(error, "REGISTRATION_ERROR");
