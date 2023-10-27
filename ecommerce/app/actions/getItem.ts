b842de23785e47fa31f3ffe06321f36e96d06043
河合桜汰 <o.k.20020929@icloud.com>
2023-10-27 10:13:43 +0900

    dbから値を取れないことの調査

--- ecommerce/app/actions/getItem.ts
+++ ecommerce/app/actions/getItem.ts
@@ -7,6 +7,7 @@ const getItem = async () => {
         createdAt: "desc",
       },
     });
+    console.log("取得した商品情報:", products);
     return products;
   } catch (error) {
     console.error("データベースからの取得中にエラーが発生しました:", error);
--- ecommerce/app/lib/prismadb.ts
+++ ecommerce/app/lib/prismadb.ts
@@ -4,7 +4,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient({log: ["query", "info", "error"],});
+const prismadb =
+  globalThis.prisma || new PrismaClient({ log: ["query", "info", "error"] });
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
